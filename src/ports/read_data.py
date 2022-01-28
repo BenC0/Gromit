ff55294ff75c327b916ec124bb98b5f2b2addce6
BenC0 <suddensquirrel94@gmail.com>
2022-01-28 10:20:52 +0000

    Written out parse_dimensions function to return the name and values of the dimension

--- src/ports/read_data.py
+++ src/ports/read_data.py
@@ -2,11 +2,18 @@ import pandas as pd
 
 # Parses a Pandas dataframe
 # and returns `dimensions`
+# 
 # `df` must be a Pandas dataframe
+# 
 # `dimensions` - an array of each
 # dimension. A dimension is a descriptive
 # attribute or characteristic of
 # data, usually categorical in nature
-
+# 
+# GROUND TRUTH - The dimension is _always_
+# in the first column 
 def parse_dimensions(df):
-    pass
+    return {
+        "name": df.columns[0],
+        "values": list(df[df.columns[0]].values)
+    }
