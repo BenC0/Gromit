19aac1c464b4ae3ad655955c9195545b7bd6b989
BenC0 <suddensquirrel94@gmail.com>
2022-01-28 10:11:50 +0000

    Finished writing test

--- QA/ports/read_data_test.py
+++ QA/ports/read_data_test.py
@@ -1,4 +1,5 @@
 # Bring your packages onto the path
+from asyncore import read
 import sys, os
 sys.path.append('../../')
 
@@ -9,4 +10,6 @@ import src.ports.read_data as read_data
 REL_TEST_FILEPATH = "../test_files/example-ga-export.csv"
 ABS_TEST_FILEPATH = os.path.abspath(REL_TEST_FILEPATH)
 
-openedCSV = csv_reader.open_csv(ABS_TEST_FILEPATH)
+df = csv_reader.open_csv(ABS_TEST_FILEPATH)
+dimensions = read_data.parse_dimensions(df)
+print(dimensions)
