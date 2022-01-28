4e24728b85a2967ddbe615364ddf0a714aa9832c
BenC0 <suddensquirrel94@gmail.com>
2022-01-28 09:37:47 +0000

    clarified returned data type (pandas dataframe) in function comments

--- src/ports/adapters/csv_reader.py
+++ src/ports/adapters/csv_reader.py
@@ -4,6 +4,7 @@ import pandas as pd
 # Opens the CSV file specified by `path`
 # `path` can be either relative or
 # absolute filepath and must be a string
+# returns a pandas dataframe
 def open_csv(path):
     csv = pd.read_csv(path)
     return csv
