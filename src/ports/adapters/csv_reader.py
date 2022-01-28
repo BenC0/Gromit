b107d01ab04f78cbc0d180e1747cc367ef01614c
BenC0 <suddensquirrel94@gmail.com>
2022-01-28 09:35:31 +0000

    csv_reader now users pandas to open the csv

--- src/ports/adapters/csv_reader.py
+++ src/ports/adapters/csv_reader.py
@@ -1,8 +1,9 @@
-
+import pandas as pd
 
 
 # Opens the CSV file specified by `path`
 # `path` can be either relative or
 # absolute filepath and must be a string
 def open_csv(path):
-    pass
+    csv = pd.read_csv(path, header=None, names=range(3))
+    return csv
