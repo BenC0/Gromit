abfd967c834e820109183571e15cc77dfbd0e77b
BenC0 <suddensquirrel94@gmail.com>
2022-01-28 10:07:08 +0000

    Created read_data port and started fleshing out the parse_dimensions function

---
+++ src/ports/read_data.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# Parses a Pandas dataframe
+# and returns `dimensions`
+# `df` must be a Pandas dataframe
+# `dimensions` - an array of each
+# dimension. A dimension is a descriptive
+# attribute or characteristic of
+# data, usually categorical in nature
+
+def parse_dimensions(df):
+    pass
