788002ca18096a225204673882df3ccba0c4f715
BenC0 <suddensquirrel94@gmail.com>
2022-01-28 10:10:27 +0000

    moved example report to generic test_files dir

--- QA/adapters/csv_reader_test.py
+++ QA/adapters/csv_reader_test.py
@@ -5,7 +5,7 @@ sys.path.append('../../')
 # import csv_reader.py
 import src.ports.adapters.csv_reader as csv_reader
 
-REL_TEST_FILEPATH = "./example-ga-export.csv"
+REL_TEST_FILEPATH = "../test_files/example-ga-export.csv"
 ABS_TEST_FILEPATH = os.path.abspath(REL_TEST_FILEPATH)
 
 openedCSV = csv_reader.open_csv(ABS_TEST_FILEPATH)
