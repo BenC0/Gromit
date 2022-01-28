6c5786d4db7299e36fbb05532f1c90a368b36ec0
BenC0 <suddensquirrel94@gmail.com>
2022-01-28 09:35:10 +0000

    Fixed the example export filepath

--- QA/adapters/csv_reader_test.py
+++ QA/adapters/csv_reader_test.py
@@ -5,8 +5,9 @@ sys.path.append('../../')
 # import csv_reader.py
 import src.ports.adapters.csv_reader as csv_reader
 
-TEST_FILEPATH = "./example-ga-report.csv"
+REL_TEST_FILEPATH = "./example-ga-export.csv"
+ABS_TEST_FILEPATH = os.path.abspath(REL_TEST_FILEPATH)
 
-openedCSV = csv_reader.open_csv(TEST_FILEPATH)
+openedCSV = csv_reader.open_csv(ABS_TEST_FILEPATH)
 
 print(openedCSV)
